e7319b303dec8d0292a4ab14c55db1751fe1ca2c
stephuofa <yearley@ualberta.ca>
2024-10-22 11:25:54 -0600

    implement device recognition and navigation btw launch,registration, and home rename main to launch

---
+++ app/PickMe_nebula0/app/src/main/java/com/example/pickme_nebula0/activities/HomePageActivity.java
@@ -0,0 +1,16 @@
+package com.example.pickme_nebula0.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.pickme_nebula0.R;
+
+public class HomePageActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+
+}
