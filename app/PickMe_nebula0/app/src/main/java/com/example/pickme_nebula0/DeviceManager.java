fdeee4d72db9daabe311a9a133f62e2e482dbd53
stephuofa <yearley@ualberta.ca>
2024-10-21 20:50:54 -0600

    implement basic start sequence, needs DB

--- app/PickMe_nebula0/app/src/main/java/com/example/pickme_nebula0/DeviceManager.java
+++ app/PickMe_nebula0/app/src/main/java/com/example/pickme_nebula0/DeviceManager.java
@@ -3,11 +3,21 @@ package com.example.pickme_nebula0;
 import android.content.Context;
 import android.provider.Settings;
 
+/**
+ * @author : Stephine Yearley
+ */
 public class DeviceManager {
 
     // modified based on code produced by ChatGPT-4o by OpenAI in response to the prompt:
     // "in android studio using java, I want to get the user's device id"
     // generated on Oct 20th, 2024
+
+    /**
+     * Gets string identifier of physical device, this is unique to each physical device.
+     *
+     * @param context
+     * @return string containing hardware id provided by Andriod
+     */
     public static String getDeviceId(Context context) {
         return Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
     }
--- app/PickMe_nebula0/app/src/main/java/com/example/pickme_nebula0/activities/MainActivity.java
+++ app/PickMe_nebula0/app/src/main/java/com/example/pickme_nebula0/activities/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.pickme_nebula0.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,9 +9,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.pickme_nebula0.DeviceManager;
 import com.example.pickme_nebula0.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    String deviceID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +26,19 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Check if the user has registered
+        deviceID = DeviceManager.getDeviceId(this);
+        // TODO: query DB to see if user has registered or if its their first time
+        boolean registered = false;
+        if (!registered){ // if not registered, send them to enter their user info
+            Intent intent = new Intent(MainActivity.this, UserInfoActivity.class);
+            intent.putExtra("newUser", true);
+            startActivity(intent);
+        }
+
+        // User is registered, render homepage
+        // TODO
+
     }
 }
