98ddd18b76660bec97416ab08711f74da7479060
Taekwan Yoon <taekwan@ualberta.ca>
2024-10-27 17:38:37 -0600

    added choose role buttons in the homePage

--- app/PickMe_nebula0/app/src/main/java/com/example/pickme_nebula0/activities/HomePageActivity.java
+++ app/PickMe_nebula0/app/src/main/java/com/example/pickme_nebula0/activities/HomePageActivity.java
@@ -1,16 +1,62 @@
 package com.example.pickme_nebula0.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.pickme_nebula0.R;
 
 public class HomePageActivity extends AppCompatActivity {
+    Button adminButton;
+    Button entrantButton;
+    Button organizerButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        adminButton = findViewById(R.id.adminButton);
+        entrantButton = findViewById(R.id.entrantButton);
+        organizerButton = findViewById(R.id.organizerButton);
+
+        adminButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigateToAdminHomePage();
+            }
+        });
+
+        entrantButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigateToEntrantHomePage();
+            }
+        });
+
+        organizerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigateToOrganizerHomePage();
+            }
+        });
     }
 
+    private void navigateToAdminHomePage() {
+        Intent intent = new Intent(HomePageActivity.this, AdminHomeActivity.class);
+        startActivity(intent);
+    }
+
+    private void navigateToEntrantHomePage() {
+        Intent intent = new Intent(HomePageActivity.this, EntrantHomeActivity.class);
+        startActivity(intent);
+    }
+
+    private void navigateToOrganizerHomePage() {
+        Intent intent = new Intent(HomePageActivity.this, OrganizerHomeActivity.class);
+        startActivity(intent);
+    }
 }
