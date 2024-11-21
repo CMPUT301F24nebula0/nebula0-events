d263b4075b96360700cbd83f554766056aa39a12
Taekwan Yoon <taekwan@ualberta.ca>
2024-11-21 00:50:52 -0700

    once register button clicked, user's geolocation data stored in database. Now, I just need to show this geolocation on a map

--- app/PickMe_nebula0/app/build.gradle.kts
+++ app/PickMe_nebula0/app/build.gradle.kts
@@ -66,5 +66,5 @@ dependencies {
     implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.viewpager2:viewpager2:1.0.0")
     implementation("androidx.core:core-ktx:1.10.0")
-
+    implementation("com.google.android.gms:play-services-location:21.0.1")
 }
