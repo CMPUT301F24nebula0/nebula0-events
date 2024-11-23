b1504c66317fb21c47d4ee15fa78aa80b586c5f9
Taekwan Yoon <taekwan@ualberta.ca>
2024-11-23 11:41:41 -0700

    added back button for the map and refactored several files

--- app/PickMe_nebula0/app/src/main/java/com/example/pickme_nebula0/GoogleMapActivity.java
+++ app/PickMe_nebula0/app/src/main/java/com/example/pickme_nebula0/GoogleMapActivity.java
@@ -2,7 +2,6 @@ package com.example.pickme_nebula0;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -16,8 +15,12 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+/**
+ * Class to display a map with a marker at a given latitude and longitude.
+ *
+ * @author Taekwan Yoon
+ */
 public class GoogleMapActivity extends FragmentActivity implements OnMapReadyCallback {
-    private GoogleMap mMap;
     double latitude;
     double longitude;
     Button backButton;
@@ -26,14 +29,39 @@ public class GoogleMapActivity extends FragmentActivity implements OnMapReadyCal
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_map);
+        setContentView(R.layout.activity_map); // attach map component to layout
 
+        // initialize map
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map_fragment);
 
-        latitude = getIntent().getDoubleExtra("latitude", 50.7749);
-        longitude = getIntent().getDoubleExtra("longitude", -122.4194);
+        displayMap(mapFragment); // display map
 
+        setLatLngFromIntent(); // set latitude and longitude from intent
+
+        backButton(); // back button
+    }
+
+    /**
+     * When map is ready, add marker to selected location and move camera to selected location.
+     *
+     * @param googleMap google map
+     */
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        LatLng location = new LatLng(latitude, longitude);
+        // add marker to selected location
+        googleMap.addMarker(new MarkerOptions().position(location).title("Selected Location"));
+        // move camera to selected location
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15));
+    }
+
+    /**
+     * Display map.
+     *
+     * @param mapFragment map fragment
+     */
+    private void displayMap(SupportMapFragment mapFragment) {
         if (mapFragment != null) {
             mapFragment.getMapAsync(this);
         } else {
@@ -41,24 +69,24 @@ public class GoogleMapActivity extends FragmentActivity implements OnMapReadyCal
             Log.e("MapActivity", "SupportMapFragment is null");
             finish();
         }
+    }
 
+    /**
+     * Get latitude and longitude from intent.
+     */
+    private void setLatLngFromIntent() {
+        latitude = getIntent().getDoubleExtra("latitude", 50.7749);
+        longitude = getIntent().getDoubleExtra("longitude", -122.4194);
+    }
+
+    /**
+     * Attach back button and its logic.
+     */
+    private void backButton() {
+        // back button
         backButton = findViewById(R.id.back_button);
-
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getOnBackPressedDispatcher().onBackPressed();
-            }
-        });
+        // back button logic
+        backButton.setOnClickListener(v -> getOnBackPressedDispatcher().onBackPressed());
     }
-
-    @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
-        mMap = googleMap;
-        LatLng location = new LatLng(latitude, longitude);
-        mMap.addMarker(new MarkerOptions().position(location).title("Selected Location"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15));
-    }
-
 }
 
