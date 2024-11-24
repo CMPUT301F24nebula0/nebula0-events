f33dc8bde33a43ef080c36339e3a4c7b51453184
stephuofa <yearley@ualberta.ca>
2024-11-24 14:31:21 -0700

    organizer add,view,change poster

--- app/PickMe_nebula0/app/src/main/java/com/example/pickme_nebula0/db/FBStorageManager.java
+++ app/PickMe_nebula0/app/src/main/java/com/example/pickme_nebula0/db/FBStorageManager.java
@@ -12,6 +12,7 @@ import androidx.core.content.ContextCompat;
 
 import com.example.pickme_nebula0.DeviceManager;
 import com.example.pickme_nebula0.SharedDialogue;
+import com.example.pickme_nebula0.event.Event;
 import com.example.pickme_nebula0.user.User;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.storage.FirebaseStorage;
@@ -35,7 +36,6 @@ public class FBStorageManager {
         Toast failureToast = Toast.makeText(context,"Could not update profile picture",Toast.LENGTH_LONG);
         UploadTask uploadTask = newFileRef.putFile(uri);
         uploadTask.addOnFailureListener(e -> {
-                    SharedDialogue.showInvalidDataAlert("Could not update profile picture",context);
                     failureToast.show();
                 })
                 .addOnSuccessListener(taskSnapshot -> {
@@ -51,13 +51,34 @@ public class FBStorageManager {
                 });
     }
 
+    public static void uploadPoster(Uri uri,String eventID, Context context){
+        StorageReference newFileRef = storageRef.child("eventPosters/"+eventID);
+
+        Toast failureToast = Toast.makeText(context,"Could not upload poster",Toast.LENGTH_LONG);
+        UploadTask uploadTask = newFileRef.putFile(uri);
+        uploadTask.addOnFailureListener(e -> {
+                    failureToast.show();
+                })
+                .addOnSuccessListener(taskSnapshot -> {
+                    Toast.makeText(context, "File uploaded :)", Toast.LENGTH_SHORT).show();
+                    newFileRef.getDownloadUrl().addOnSuccessListener(accessUri -> {
+                        // Add to db - TODO turn this into a DB manager function
+                        DBManager dbManager = new DBManager();
+                        DocumentReference userDocRef = dbManager.db.collection("Events").document(eventID);
+                        dbManager.updateField(userDocRef,"poster",accessUri);
+                    }).addOnFailureListener(exception -> {
+                        failureToast.show();
+                    });
+                });
+    }
+
     public static void uploadProfilePic(String filePath, String userID, Context context){
         Uri uri = Uri.fromFile(new File(filePath));
         uploadProfilePic(uri,userID,context);
     }
 
     public static void retrieveProfilePicUri(String userID, Uri2VoidCallback onSuccessCallback, DBManager.Void2VoidCallback onFailureCallback){
-            dbManager.getUser(DeviceManager.getDeviceId(), userObj -> {
+            dbManager.getUser(userID, userObj -> {
                 User user = (User) userObj;
                 if (user.getProfilePic() != null){
                     Uri uri = Uri.parse(user.getProfilePic());
@@ -67,6 +88,19 @@ public class FBStorageManager {
                 }
 
             }, onFailureCallback);
+    }
+
+    public static void retrievePosterUri(String eventID, Uri2VoidCallback onSuccessCallback, DBManager.Void2VoidCallback onFailureCallback){
+        dbManager.getEvent(eventID, eventObj -> {
+            Event event = (Event) eventObj;
+            if (event.getPoster() != null){
+                Uri uri = Uri.parse(event.getPoster());
+                onSuccessCallback.run(uri);
+            }else{
+                onFailureCallback.run();
+            }
+
+        }, onFailureCallback);
     }
 
     }
